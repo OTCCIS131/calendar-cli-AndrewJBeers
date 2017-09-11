954d15385480cde1eea754a8f7a1d0e45b2b37fe
Unknown <ajbeers89@gmail.com>
2017-09-11 13:26:16 -0500

    Working product from end of class

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const moment = MomentRange.extendMoment(Moment)
 const chalk = require('chalk')
 const _ = require('lodash')
 
-let year = moment().range('year')
-    console.log(year)
+let year = moment().range('year');
 
 
+//let now = moment()
+//let month = now.range('month')
 
+_.forEach(Array.from(year.by('months')), month => {
+    console.log(_.pad(month.format('MMM'), 26, " "))
+    console.log('S   M   T   W   TH  F   S   ')
 
+    let monthRange = month.range('month')
+    let firstDay = monthRange.start.day()
+    console.log(firstDay)
 
-
+})
 
 
 
