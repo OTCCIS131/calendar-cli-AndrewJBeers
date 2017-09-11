cc22fc8d3ccfaa1c8b3e4d0ce5b1eb347aade61c
Unknown <ajbeers89@gmail.com>
2017-09-11 13:09:52 -0500

    consts loaded and working

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-
-
+const Moment = require('moment')
+const MomentRange = require('moment-range')
+const moment = MomentRange.extendMoment(Moment)
+const chalk = require('chalk')
+const _ = require('lodash')
 
 let year = moment().range('year')
     console.log(year)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
