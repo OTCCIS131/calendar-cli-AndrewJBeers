b54aa86451dbc9c2ee804df7d09b8f7bd43678fc
Unknown <ajbeers89@gmail.com>
2017-09-17 22:05:09 -0500

    working

--- index.js
+++ index.js
@@ -4,51 +4,34 @@ const moment = MomentRange.extendMoment(Moment)
 const chalk = require('chalk')
 const _ = require('lodash')
 
-// let year = moment().range('year');
-
-
-//let now = moment()
-// let month = now.range('month')
-
-// _.forEach(Array.from(year.by('months')), month => {
-//     console.log(_.pad(month.format('MMM'), 26, " "))
-//     console.log('S   M   T   W   TH  F   S   ')
-
-//     let monthRange = month.range('month')
-//     let firstDay = monthRange.start.day()
-//     console.log(firstDay)
-
-// })
-
-
-
 let now = moment()
 console.log(now.year())
 let year = now.range('year')
+let month = now.range('months')
 for(const month of year.by('months'))
     {
         console.log(_.pad(month.format('MMMM'), 25, '-'))
-         console.log('S   M   T   W   TH  F   S   ')
- 
-    let days = Array.from(month.range('month').by('days'))
-    
-    let paddedDays = _.map(days, day => {
-        let date = day.date()
-        if (day.month()== 9 && day.date() == 10){
-            date = chalk.red(date)
-        }
-        return _.padEnd(date, 2, ' ')
-    })
-    console.log(paddedDays)
-    
-    //     console.log(days)
-    // _.chain(days)
+        console.log('S   M   T   W   TH  F   S   ')
         
-    //     .map(day => {
-    //         console.log('here')
-    //         console.log(day)
-    //     })
+        let days = Array.from(month.range('month').by('days'))
+        startOfMonth = days[0].format('e')
+        let paddedDays = _.map(days, day => 
+            {
+            let date = day.date()
+            if (day.month()== 8 && day.date() == 10){
+                date = chalk.red(date)
+            }
+            if (day.month() == 9 && day.date() == 12){
+                date = chalk.blue(date)
+            }
+            return _.padEnd(date, 2, ' ')
+        })
+        for ( i = startOfMonth; i > 0; i-- ){
+            paddedDays.unshift("  ")
+        }
+        paddedDays = _.chunk(paddedDays, 7)
+        for (i = 0; i<paddedDays.length; i ++){
+            console.log(paddedDays[i].join('  '))
+        }
+        console.log('\n')
     }
-
-
-
